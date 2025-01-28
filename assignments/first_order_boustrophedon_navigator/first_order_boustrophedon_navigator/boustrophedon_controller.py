f0a47e1fd889c17bd7f86062024037fbfb4cbe7f
Akshay Goenka <99144925+2WinTale@users.noreply.github.com>
2025-01-27 21:27:38 -0700

    Update boustrophedon_controller.py
    Kp_linear was changed to make the turtle sim more efficient
    Kd_linear was changed for a smaller path and turns
    Kp_angular and Kd_angular were changed for better and sharp turns and better stabilization
    spacing was changed so that fewer paths followed and more area was covered.

--- assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
+++ assignments/first_order_boustrophedon_navigator/first_order_boustrophedon_navigator/boustrophedon_controller.py
@@ -19,11 +19,11 @@ class BoustrophedonController(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('Kp_linear', 10.0),
-                ('Kd_linear', 0.1),
-                ('Kp_angular', 5.0),
-                ('Kd_angular', 0.2),
-                ('spacing', 1.0)
+                ('Kp_linear', 2.8),
+                ('Kd_linear', 0.05),
+                ('Kp_angular', 12.0),
+                ('Kd_angular', 0.008),
+                ('spacing', 1.4)
             ]
         )
         
